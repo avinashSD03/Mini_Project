78dc7054bdb0c27d20f62b006dd7afc4180c374f
Avinash Dixit <avinashdixit9587@gmail.com>
2024-04-02 19:49:02 +0530

    3rd Commit

--- backend/index.js
+++ backend/index.js
@@ -30,11 +30,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "mini_Project",
-    password: "Chidambar@03",
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
   });
   db.connect();
 
